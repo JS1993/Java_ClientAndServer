62979b6bd009db8cc98b3051a37d9e0b632baece
jiangsuhjq <jiangsuhjq@163.com>
2016-12-16 12:02:54 +0800

    完成功能

--- src/com/jiangsu/ClientAndServer/Client.java
+++ src/com/jiangsu/ClientAndServer/Client.java
@@ -1,10 +1,29 @@
 package com.jiangsu.ClientAndServer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class Client {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	/**
+	 * 客户端向服务器写字符串（键盘录入），服务器（多线程）将字符串反转后写回，客户端再次读取的是反转后的字符串
+	 * @param args
+	 * @throws IOException 
+	 * @throws UnknownHostException 
+	 */
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		Scanner sc = new Scanner(System.in);
+		Socket socket = new Socket("127.0.0.1",54321);
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		PrintStream ps = new PrintStream(socket.getOutputStream());
+		
+		ps.println(sc.nextLine());
 	}
 
 }
--- src/com/jiangsu/ClientAndServer/Server.java
+++ src/com/jiangsu/ClientAndServer/Server.java
@@ -1,10 +1,37 @@
 package com.jiangsu.ClientAndServer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		ServerSocket server = new ServerSocket(54321);
+		System.out.println("服务器启动，绑定54321端口");
 
+		while(true){
+			final Socket socket = server.accept();
+			new Thread(){
+				public void run(){
+					try {
+						BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+						PrintStream ps = new PrintStream(socket.getOutputStream());
+						
+						String line = br.readLine();		//读取客户端写过来的数据
+						line = new StringBuilder(line).reverse().toString();
+						ps.println(line);
+						socket.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}.start();
+			
+		}
 	}
 
 }
